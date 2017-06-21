dc97dd7b7bae764848e814be598420928be43332
Behnam Esfahbod <behnam@zwnj.org>
2017-06-20 22:41:30 -0600

    [README] Expand example

--- examples/basics.rs
+++ examples/basics.rs
@@ -1,23 +1,14 @@
-// Copyright 2015 The Servo Project Developers.
-// Copyright 2017 The UNIC Project Developers.
-//
-// See the COPYRIGHT file at the top-level directory of this distribution.
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
-#![forbid(unsafe_code)]
-
 extern crate unic;
 
-
+use unic::bidi::BidiInfo;
+use unic::normal::StrNormalForm;
 use unic::ucd::bidi::{BidiClass, BidiChar, BidiStr};
-
+use unic::ucd::normal::compose;
 
 fn main() {
+
+    // Bidi
+
     let text = concat![
         "א",
         "ב",
@@ -40,4 +31,34 @@ fn main() {
     assert!(!text.chars().nth(3).unwrap().is_explicit());
     assert!(text.chars().nth(3).unwrap().is_ltr());
     assert!(!text.chars().nth(3).unwrap().is_rtl());
+
+    let bidi_info = BidiInfo::new(&text, None);
+    assert_eq!(bidi_info.paragraphs.len(), 1);
+
+    let para = &bidi_info.paragraphs[0];
+    assert_eq!(para.level.number(), 1);
+    assert_eq!(para.level.is_rtl(), true);
+
+    let line = para.range.clone();
+    let display = bidi_info.reorder_line(para, line);
+    assert_eq!(
+        display,
+        concat![
+            "a",
+            "b",
+            "c",
+            "ג",
+            "ב",
+            "א",
+        ]
+    );
+
+    // Normalization
+
+    assert_eq!(compose('A', '\u{30a}'), Some('Å'));
+
+    let s = "ÅΩ";
+    let c = s.nfc().collect::<String>();
+    assert_eq!(c, "ÅΩ");
+
 }
