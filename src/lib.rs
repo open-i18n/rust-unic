f66e2390719055b5150de479f0f04e5c4d0c783b
Behnam Esfahbod <behnam@zwnj.org>
2017-06-21 14:25:52 -0600

    [src/lib.rs] Add basic example to create docblock

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,74 @@
 //! -   [`normal`](/unic-normal): Unicode Normalization Forms (USA\#15).
 //!
 //! -   [`idna`](/unic-idna): Unicode IDNA Compatibility Processing (UTS\#46).
+//!
+//!
+//! ## A Basic Example
+//!
+//! ```rust
+//! extern crate unic;
+//!
+//! use unic::bidi::BidiInfo;
+//! use unic::normal::StrNormalForm;
+//! use unic::ucd::bidi::{BidiClass, BidiChar, BidiStr};
+//! use unic::ucd::normal::compose;
+//!
+//! fn main() {
+//!     // Bidi
+//!
+//!     let text = concat![
+//!         "א",
+//!         "ב",
+//!         "ג",
+//!         "a",
+//!         "b",
+//!         "c",
+//!     ];
+//!
+//!     assert!(!text.has_explicit());
+//!     assert!(text.has_rtl());
+//!     assert!(text.has_ltr());
+//!
+//!     assert_eq!(text.chars().nth(0).unwrap().bidi_class(), BidiClass::R);
+//!     assert!(!text.chars().nth(0).unwrap().is_explicit());
+//!     assert!(!text.chars().nth(0).unwrap().is_ltr());
+//!     assert!(text.chars().nth(0).unwrap().is_rtl());
+//!
+//!     assert_eq!(text.chars().nth(3).unwrap().bidi_class(), BidiClass::L);
+//!     assert!(!text.chars().nth(3).unwrap().is_explicit());
+//!     assert!(text.chars().nth(3).unwrap().is_ltr());
+//!     assert!(!text.chars().nth(3).unwrap().is_rtl());
+//!
+//!     let bidi_info = BidiInfo::new(&text, None);
+//!     assert_eq!(bidi_info.paragraphs.len(), 1);
+//!
+//!     let para = &bidi_info.paragraphs[0];
+//!     assert_eq!(para.level.number(), 1);
+//!     assert_eq!(para.level.is_rtl(), true);
+//!
+//!     let line = para.range.clone();
+//!     let display = bidi_info.reorder_line(para, line);
+//!     assert_eq!(
+//!         display,
+//!         concat![
+//!             "a",
+//!             "b",
+//!             "c",
+//!             "ג",
+//!             "ב",
+//!             "א",
+//!         ]
+//!     );
+//!
+//!     // Normalization
+//!
+//!     assert_eq!(compose('A', '\u{30a}'), Some('Å'));
+//!
+//!     let s = "ÅΩ";
+//!     let c = s.nfc().collect::<String>();
+//!     assert_eq!(c, "ÅΩ");
+//! }
+//! ```
 
 pub extern crate unic_bidi as bidi;
 pub extern crate unic_idna as idna;
