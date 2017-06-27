8944bb95d622371d91dc9da8c15a2907d2a86dcc
Behnam Esfahbod <behnam@zwnj.org>
2017-06-26 19:04:09 -0600

    [ucd/age] Add more tests for Age vs UNICODE_VERSION

--- components/ucd/src/lib.rs
+++ components/ucd/src/lib.rs
@@ -42,4 +42,4 @@ pub const PKG_DESCRIPTION: &'static str = env!("CARGO_PKG_DESCRIPTION");
 
 pub use age::{Age, CharAge};
 pub use bidi::{BidiClass, CharBidiClass, StrBidiClass};
-pub use normal::{CanonicalCombiningClass};
+pub use normal::CanonicalCombiningClass;
