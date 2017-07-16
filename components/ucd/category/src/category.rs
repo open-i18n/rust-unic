55ed6babef204f886033de99403aa40b1f7b3df6
CAD97 <cad97@cad97.com>
2017-07-16 02:12:55 -0400

    Format pass

--- components/ucd/category/src/category.rs
+++ components/ucd/category/src/category.rs
@@ -10,94 +10,66 @@ use std::cmp::Ordering;
 pub enum GeneralCategory {
     /// An uppercase letter (Short form: `Lu`)
     UppercaseLetter,
-
     /// A lowercase letter (Short form: `Ll`)
     LowercaseLetter,
-
     /// A digraphic character, with first part uppercase (Short form: `Lt`)
     TitlecaseLetter,
-
     /// A modifier letter (Short form: `Lm`)
     ModifierLetter,
-
     /// Other letters, including syllables and ideographs (Short form: `Lo`)
     OtherLetter,
-
     /// A nonspacing combining mark (zero advance width) (Short form: `Mn`)
     NonspacingMark,
-
     /// A spacing combining mark (positive advance width) (Short form: `Mc`)
     SpacingMark,
-
     /// An enclosing combining mark (Short form: `Me`)
     EnclosingMark,
-
     /// A decimal digit (Short form: `Nd`)
     DecimalNumber,
-
     /// A letterlike numeric character (Short form: `Nl`)
     LetterNumber,
-
     /// A numeric character of other type (Short form: `No`)
     OtherNumber,
-
     /// A connecting punctuation mark, like a tie (Short form: `Pc`)
     ConnectorPunctuation,
-
     /// A dash or hyphen punctuation mark (Short form: `Pd`)
     DashPunctuation,
-
     /// An opening punctuation mark (of a pair) (Short form: `Ps`)
     OpenPunctuation,
-
     /// A closing punctuation mark (of a pair) (Short form: `Pe`)
     ClosePunctuation,
-
     /// An initial quotation mark (Short form: `Pi`)
     InitialPunctuation,
-
     /// A final quotation mark (Short form: `Pf`)
     FinalPunctuation,
-
     /// A punctuation mark of other type (Short form: `Po`)
     OtherPunctuation,
-
     /// A symbol of mathematical use (Short form: `Sm`)
     MathSymbol,
-
     /// A currency sign (Short form: `Sc`)
     CurrencySymbol,
-
     /// A non-letterlike modifier symbol (Short form: `Sk`)
     ModifierSymbol,
-
     /// A symbol of other type (Short form: `So`)
     OtherSymbol,
-
     /// A space character (of various non-zero widths) (Short form: `Zs`)
     SpaceSeparator,
-
     /// U+2028 LINE SEPARATOR only (Short form: `Zl`)
     LineSeparator,
-
     /// U+2029 PARAGRAPH SEPARATOR only (Short form: `Zp`)
     ParagraphSeparator,
-
     /// A C0 or C1 control code (Short form: `Cc`)
     Control,
-
     /// A format control character (Short form: `Cf`)
     Format,
-
     /// A surrogate code point (Short form: `Cs`)
     Surrogate,
-
     /// A private-use character (Short form: `Co`)
     PrivateUse,
-
     /// Unassigned (Short form: `Cn`)
     Unassigned,
 }
+
 use self::GeneralCategory::*;
 
 const GENERAL_CATEGORY_TABLE: &'static [(char, char, GeneralCategory)] =
@@ -115,7 +87,6 @@ impl GeneralCategory {
     pub fn is_cased_letter(&self) -> bool {
         matches!(*self, UppercaseLetter | LowercaseLetter | TitlecaseLetter)
     }
-
     /// `Lu` | `Ll` | `Lt` | `Lm` | `Lo`  (Short form: `L`)
     pub fn is_letter(&self) -> bool {
         matches!(
@@ -123,17 +94,14 @@ impl GeneralCategory {
             UppercaseLetter | LowercaseLetter | TitlecaseLetter | ModifierLetter | OtherLetter
         )
     }
-
     /// `Mn` | `Mc` | `Me`  (Short form: `M`)
     pub fn is_mark(&self) -> bool {
         matches!(*self, NonspacingMark | SpacingMark | EnclosingMark)
     }
-
     /// `Nd` | `Nl` | `No`  (Short form: `N`)
     pub fn is_number(&self) -> bool {
         matches!(*self, DecimalNumber | LetterNumber | OtherNumber)
     }
-
     /// `Pc` | `Pd` | `Ps` | `Pe` | `Pi` | `Pf` | `Po`  (Short form: `P`)
     pub fn is_punctuation(&self) -> bool {
         matches!(
@@ -142,7 +110,6 @@ impl GeneralCategory {
                 InitialPunctuation | FinalPunctuation | OtherPunctuation
         )
     }
-
     /// `Sm` | `Sc` | `Sk` | `So`  (Short form: `S`)
     pub fn is_symbol(&self) -> bool {
         matches!(
@@ -150,12 +117,10 @@ impl GeneralCategory {
             MathSymbol | CurrencySymbol | ModifierLetter | OtherSymbol
         )
     }
-
     /// `Zs` | `Zl` | `Zp`  (Short form: `Z`)
     pub fn is_separator(&self) -> bool {
         matches!(*self, SpaceSeparator | LineSeparator | ParagraphSeparator)
     }
-
     /// `Cc` | `Cf` | `Cs` | `Co` | `Cn`  (Short form: `C`)
     pub fn is_other(&self) -> bool {
         matches!(
