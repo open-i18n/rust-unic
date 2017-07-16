4d3cc7472d153bbca7bcedc2ae45ce08f095c511
CAD97 <cad97@cad97.com>
2017-07-16 19:48:25 -0400

    Clean up common
    Our relative root (the prefix if we do Path::new) is our project root.
    
    So let's keep the concerns local and let the module specific code construct the path.

--- gen/src/utils/common.rs
+++ gen/src/utils/common.rs
@@ -1,71 +1,30 @@
-use std::path::{Path, PathBuf};
 use std::process::Command;
+use std::path::Path;
 use std::{fs, io};
 
-pub struct Dir {
-    root: &'static Path,
-    ucd_data: PathBuf,
-    ucd_test_data: PathBuf,
-    idna_data: PathBuf,
-    idna_test_data: PathBuf,
+pub fn cleanup_data<P>(path: P) -> io::Result<()>
+where
+    P: AsRef<Path>,
+{
+    fs::remove_dir_all(&path)?;
+    fs::create_dir(&path)?;
+    Ok(())
 }
 
-impl Dir {
-    pub fn root(&self) -> &Path {
-        &self.root
-    }
-    pub fn ucd_data(&self) -> &Path {
-        &self.ucd_data
-    }
-    pub fn ucd_test_data(&self) -> &Path {
-        &self.ucd_test_data
-    }
-    pub fn idna_data(&self) -> &Path {
-        &self.idna_data
-    }
-    pub fn idna_test_data(&self) -> &Path {
-        &self.idna_test_data
-    }
-}
-
-impl Default for Dir {
-    fn default() -> Dir {
-        let root = Path::new("../../../");
-        Dir {
-            root: root,
-            ucd_data: root.join("data/ucd"),
-            ucd_test_data: root.join("data/ucd/test"),
-            idna_data: root.join("data/idna"),
-            idna_test_data: root.join("data/idna/test"),
-        }
-    }
-}
-
-impl Dir {
-    pub fn cleanup_data<P>(path: P) -> io::Result<()>
-    where
-        P: AsRef<Path>,
-    {
-        fs::remove_dir_all(&path)?;
-        fs::create_dir(&path)?;
-        Ok(())
-    }
-
-    pub fn cleanup_rsv<P>(path: P) -> io::Result<()>
-    where
-        P: AsRef<Path>,
-    {
-        for entry in fs::read_dir(path)? {
-            let path = entry?.path();
-            if let Some(ext) = path.extension() {
-                if ext.to_string_lossy() == "rsv" {
-                    fs::remove_file(&path)?;
-                }
+pub fn cleanup_rsv<P>(path: P) -> io::Result<()>
+where
+    P: AsRef<Path>,
+{
+    for entry in fs::read_dir(path)? {
+        let path = entry?.path();
+        if let Some(ext) = path.extension() {
+            if ext.to_string_lossy() == "rsv" {
+                fs::remove_file(&path)?;
             }
         }
-
-        Ok(())
     }
+
+    Ok(())
 }
 
 pub fn fetch(url: &str, destination: &Path) {
