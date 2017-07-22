95adc3d182c7396984db86e1fce72f4784c009ea
CAD97 <cad97@cad97.com>
2017-07-22 18:46:48 -0400

    Add unic-ucd-bidi table generation
    Signed-off-by: CAD97 <cad97@cad97.com>

---
+++ gen/src/generate/ucd/bidi.rs
@@ -0,0 +1,70 @@
+use std::char;
+use std::fs::File;
+use std::io::{self, Write};
+use std::path::Path;
+
+use super::UnicodeDataEntry;
+
+use generate::PREAMBLE;
+use generate::char_property::char_map::*;
+
+#[derive(Clone, Default, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+struct BidiData<'a>(BTreeMap<char, &'a str>);
+
+impl<'a> BidiData<'a> {
+    fn emit<P: AsRef<Path>>(&self, dir: P) -> io::Result<()> {
+        let BidiData(ref map) = *self;
+        let mut file = File::create(dir.as_ref().join("bidi_class_values.rsv"))?;
+        writeln!(file, "{}\n{}", PREAMBLE, map.to_bsearch_table_default())?;
+        Ok(())
+    }
+}
+
+impl<'a, I> From<I> for BidiData<'a>
+where
+    I: Iterator<Item = &'a UnicodeDataEntry>,
+{
+    fn from(it: I) -> Self {
+        // Default Bidi_Class for unassigned codepoints
+        // <http://www.unicode.org/Public/UNIDATA/extracted/DerivedBidiClass.txt>
+        let defaults = &[
+            (0x0600, 0x07BF, "AL"),
+            (0x08A0, 0x08FF, "AL"),
+            (0xFB50, 0xFDCF, "AL"),
+            (0xFDF0, 0xFDFF, "AL"),
+            (0xFE70, 0xFEFF, "AL"),
+            (0x1EE00, 0x1EEFF, "AL"),
+            (0x0590, 0x05FF, "R"),
+            (0x07C0, 0x089F, "R"),
+            (0xFB1D, 0xFB4F, "R"),
+            (0x10800, 0x10FFF, "R"),
+            (0x1E800, 0x1EDFF, "R"),
+            (0x1EF00, 0x1EFFF, "R"),
+            (0x20A0, 0x20CF, "ET"),
+        ];
+        let mut map = BTreeMap::<char, &str>::new();
+
+        for &UnicodeDataEntry { codepoint, ref bidi_class, .. } in it {
+            if let Some(c) = char::from_u32(codepoint) {
+                map.insert(c, bidi_class);
+            }
+        }
+
+        for &(start, end, default) in defaults {
+            for codepoint in start..(end+1) {
+                if let Some(c) = char::from_u32(codepoint) {
+                    map.entry(c).or_insert(default);
+                }
+            }
+        }
+
+        BidiData(map)
+    }
+}
+
+pub fn generate<P: AsRef<Path>>(dir: P) -> io::Result<()> {
+    super::UNICODE_VERSION.emit(&dir)?;
+    let bidi_data = BidiData::from(super::UNICODE_DATA.iter());
+    bidi_data.emit(dir)?;
+    Ok(())
+}
--- gen/src/generate/ucd/mod.rs
+++ gen/src/generate/ucd/mod.rs
@@ -1,7 +1,8 @@
 mod shared;
 
-pub mod core;
 pub mod age;
+pub mod bidi;
+pub mod core;
 
 pub use self::shared::unicode_data::{UNICODE_DATA, UnicodeDataEntry};
 pub use self::shared::version::UNICODE_VERSION;
