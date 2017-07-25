6a77e51067ee3377eb6dc33eff60f9d15c2501e4
CAD97 <cad97@cad97.com>
2017-07-25 00:38:34 -0400

    Refactor generation to delegate to the sub module
    This is in preparation of unic-idna table generation

--- gen/src/generate/ucd/mod.rs
+++ gen/src/generate/ucd/mod.rs
@@ -6,5 +6,62 @@ pub mod category;
 pub mod core;
 pub mod normal;
 
-pub use self::shared::unicode_data::{read_unicode_data, UnicodeData, UnicodeDataEntry};
-pub use self::shared::version::{read_unicode_version, UnicodeVersion};
+use std::io;
+
+pub use self::shared::unicode_data::{UnicodeData, UnicodeDataEntry};
+pub use self::shared::version::UnicodeVersion;
+
+pub fn generate_all<I, S>(crates: I) -> io::Result<()>
+where
+    I: Iterator<Item = S>,
+    S: AsRef<str>,
+{
+    println!(">>> Loading UCD Version");
+    let ucd_version = shared::version::read_unicode_version()?;
+    println!(">>> Loading UCD UnicodeData");
+    let unicode_data = shared::unicode_data::read_unicode_data()?;
+
+    for krate in crates {
+        let path = super::tables_path(krate.as_ref());
+        match krate.as_ref() {
+            "unic-ucd-age" => {
+                age::generate(
+                    path,
+                    &ucd_version,
+                    &unicode_data,
+                )?;
+            }
+            "unic-ucd-bidi" => {
+                bidi::generate(
+                    path,
+                    &ucd_version,
+                    &unicode_data,
+                )?;
+            }
+            "unic-ucd-category" => {
+                category::generate(
+                    path,
+                    &ucd_version,
+                    &unicode_data,
+                )?;
+            }
+            "unic-ucd-core" => {
+                core::generate(
+                    path,
+                    &ucd_version,
+                    &unicode_data,
+                )?;
+            }
+            "unic-ucd-normal" => {
+                normal::generate(
+                    path,
+                    &ucd_version,
+                    &unicode_data,
+                )?;
+            }
+            _ => (),
+        }
+    }
+
+    Ok(())
+}
