129b95e88c4d9fd705213d329afbcca89d7fda9d
Behnam Esfahbod <behnam@zwnj.org>
2017-08-07 21:19:18 -0700

    [etc] Move in refresh-tables.sh

--- .cargo/refresh-tables.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cargo run --release --package=unic-gen -- --download --generate idna ucd normal
-
-echo "Generating idna tables with python"
-python2 tools/gen_idna_tables.py
-echo "Generating remaining ucd tables with python"
-python2 tools/gen_ucd_tables.py
