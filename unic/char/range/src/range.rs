aee0479cbb6009769eab47b308c6c1ea17020dfa
CAD97 <cad97@cad97.com>
2017-08-12 04:41:15 -0400

    Constructors

--- unic/char/range/src/range.rs
+++ unic/char/range/src/range.rs
@@ -26,12 +26,39 @@ pub struct CharRange {
 
 /// Constructors
 impl CharRange {
-    /// Construct a closed range of characters
+    /// Construct a closed range of characters.
     pub fn closed(start: char, stop: char) -> CharRange {
         CharRange {
             low: start,
             high: stop,
         }
+    }
+
+    /// Construct a half open (right) range of characters.
+    pub fn open_right(start: char, stop: char) -> CharRange {
+        let mut range = CharRange::closed(start, stop);
+        range.step_backward();
+        range
+    }
+
+    /// Construct a half open (left) range of characters.
+    pub fn open_left(start: char, stop: char) -> CharRange {
+        let mut range = CharRange::closed(start, stop);
+        range.step_forward();
+        range
+    }
+
+    /// Construct a fully open range of characters.
+    pub fn open(start: char, stop: char) -> CharRange {
+        let mut range = CharRange::closed(start, stop);
+        range.step_forward();
+        range.step_backward();
+        range
+    }
+
+    /// Construct a range over all characters.
+    pub fn all() -> CharRange {
+        CharRange::closed('\0', char::MAX)
     }
 }
 
@@ -39,7 +66,7 @@ impl CharRange {
     #[inline]
     #[allow(unsafe_code)]
     // It is always safe to step `self.low` forward because
-    // it will only be used when less than `self.high`.
+    // `self.low` will only be used when less than `self.high`.
     fn step_forward(&mut self) {
         self.low = unsafe { step::forward(self.low) }
     }
