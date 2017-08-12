4b631466e5d96a7ac32f6b187b6706e97c86c7a6
CAD97 <cad97@cad97.com>
2017-08-11 23:37:37 -0400

    Tweak bench code

--- unic/char/range/benches/benchmarks.rs
+++ unic/char/range/benches/benchmarks.rs
@@ -6,19 +6,13 @@ extern crate unic_char_range;
 use std::char;
 use unic_char_range::*;
 
-#[inline(never)]
-fn yes_man<T>(_: &T) -> bool {
-    true
-}
-
 #[bench]
 fn count(b: &mut test::Bencher) {
-    b.iter(|| CharRange::all().iter().take_while(yes_man).count())
+    b.iter(|| CharRange::all().iter().count())
 }
 
 #[bench]
 // iterate the same range without skipping surrogates
 fn count_baseline(b: &mut test::Bencher) {
-
-    b.iter(|| (0..(char::MAX as u32 + 1)).take_while(yes_man).count())
+    b.iter(|| (0..0x110000).take_while(|_| true).count())
 }
