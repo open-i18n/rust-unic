b8236155c5b7e289f9cbe1974d2b3c188576e07b
CAD97 <cad97@cad97.com>
2017-08-11 22:49:55 -0400

    Iterator tests

--- unic/char/range/src/lib.rs
+++ unic/char/range/src/lib.rs
@@ -2,7 +2,23 @@
 //!
 //! A simple way to control iteration over a range of characters.
 //!
-//! TODO
+//! # Examples
+//!
+//! ```
+//! # #[macro_use] extern crate unic_char_range;
+//! # use unic_char_range::*;
+//!
+//! # fn main() {
+//! for character in chars!('a'..='z') {
+//!     // character is each character in the lowercase english alphabet in order
+//! }
+//!
+//! for character in CharRange::all() {
+//!     // character is every valid char from lowest codepoint to highest
+//! }
+//! # }
+//! ```
+//!
 #![forbid(bad_style, future_incompatible)]
 #![forbid(missing_debug_implementations, unconditional_recursion)]
 #![deny(missing_docs, unsafe_code, unused)]
---
+++ unic/char/range/tests/iter_tests.rs
@@ -0,0 +1,58 @@
+extern crate unic_char_range;
+
+use std::{char, u32};
+use unic_char_range::*;
+
+fn all_chars() -> Vec<char> {
+    (u32::MIN..u32::MAX)
+        .take_while(|&u| u <= char::MAX as u32)
+        .filter_map(char::from_u32)
+        .collect()
+}
+
+#[test]
+fn iter_all_chars() {
+    assert!(CharRange::all().iter().eq(all_chars()));
+}
+
+#[test]
+fn iter_all_chars_rev() {
+    assert!(CharRange::all().rev().eq(all_chars().into_iter().rev()))
+}
+
+#[test]
+fn iter_all_chars_mixed_next_back() {
+    let mut custom = CharRange::all().iter();
+    let mut simple = all_chars().into_iter();
+    while let Some(custom_char) = custom.next() {
+        assert_eq!(Some(custom_char), simple.next());
+        assert_eq!(custom.next_back(), simple.next_back());
+    }
+    assert_eq!(None, simple.next());
+}
+
+#[test]
+fn iter_fused() {
+    let mut iter = CharRange::all().iter();
+    let mut fused = all_chars().into_iter().fuse();
+    assert!(iter.by_ref().eq(fused.by_ref()));
+    for _ in 0..100 {
+        assert_eq!(iter.next(), fused.next());
+    }
+}
+
+#[test]
+fn iter_exact_trusted_len() {
+    fn assert_presents_right_len<I: ExactSizeIterator>(iter: &I, len: usize) {
+        assert_eq!(iter.len(), len);
+        assert_eq!(iter.size_hint(), (len, Some(len)));
+    }
+
+    let mut iter = CharRange::all().iter();
+    let mut predicted_length = iter.len();
+
+    while let Some(_) = iter.next() {
+        predicted_length -= 1;
+        assert_presents_right_len(&iter, predicted_length);
+    }
+}
