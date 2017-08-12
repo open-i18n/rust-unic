8f0bd863ceed77fdb7835d984f3eea4882ea7716
CAD97 <cad97@cad97.com>
2017-08-12 04:43:05 -0400

    Benchmark reversed iteration as well.
    (Oof, reverse takes three times as long on my machine
    (wheras the FilterMap is actually faster! probably branch prediction).
    I'll have to see if I missed an #[inline] or if it's something else.)

--- unic/char/range/benches/benchmarks.rs
+++ unic/char/range/benches/benchmarks.rs
@@ -7,11 +7,21 @@ use std::char;
 use unic_char_range::CharRange;
 
 #[bench]
-fn count(b: &mut test::Bencher) {
-    b.iter(|| CharRange::closed('\0', '\u{10FFFF}').count())
+fn forward_iteration(b: &mut test::Bencher) {
+    b.iter(|| CharRange::all().count())
 }
 
 #[bench]
-fn count_baseline(b: &mut test::Bencher) {
+fn forward_iteration_baseline(b: &mut test::Bencher) {
     b.iter(|| (0..0x110000).filter_map(char::from_u32).count())
+}
+
+#[bench]
+fn reverse_iteration(b: &mut test::Bencher) {
+    b.iter(|| CharRange::all().rev().count())
+}
+
+#[bench]
+fn reverse_iteration_baseline(b: &mut test::Bencher) {
+    b.iter(|| (0..0x110000).rev().filter_map(char::from_u32).count())
 }
