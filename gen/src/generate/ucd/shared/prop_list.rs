6b9aa5b8db3aafd94bcdda0a93443a7a6216c7dd
Christopher Durham <cad97@cad97.com>
2017-09-14 23:27:17 -0400

    Add missing doc comments

--- gen/src/generate/ucd/shared/prop_list.rs
+++ gen/src/generate/ucd/shared/prop_list.rs
@@ -19,39 +19,156 @@ use super::BINARY_PROPERTIES_REGEX;
 
 #[derive(Clone, Debug, Default)]
 pub struct PropList {
+    /// ASCII characters commonly used for the representation of hexadecimal numbers.
     ascii_hex_digit: BTreeSet<char>,
+
+    /// Format control characters which have specific functions in the
+    /// [Unicode Bidirectional Algorithm][UAX9].
+    ///
+    /// [UAX9]: http://unicode.org/reports/tr41/tr41-21.html#UAX9
     bidi_control: BTreeSet<char>,
+
+    /// Punctuation characters explicitly called out as dashes in the Unicode Standard,
+    /// plus their compatibility equivalents. Most of these have the `General_Category` value `Pd`,
+    /// but some have the `General_Category` value `Sm` because of their use in mathematics.
     dash: BTreeSet<char>,
+
+    /// For a machine-readable list of deprecated characters.
+    /// No characters will ever be removed from the standard,
+    /// but the usage of deprecated characters is strongly discouraged.
     deprecated: BTreeSet<char>,
+
+    /// Characters that linguistically modify the meaning of another character to which they apply.
+    /// Some diacritics are not combining characters,
+    /// and some combining characters are not diacritics.
     diacritic: BTreeSet<char>,
+
+    /// Characters whose principal function is to extend the value or shape of a preceding
+    /// alphabetic character. Typical of these are length and iteration marks.
     extender: BTreeSet<char>,
+
+    /// Characters commonly used for the representation of hexadecimal numbers,
+    /// plus their compatibility equivalents.
     hex_digit: BTreeSet<char>,
+
     // hyphen is deprecated
+    /// Characters considered to be CJKV (Chinese, Japanese, Korean, and Vietnamese)
+    /// or other siniform (Chinese writing-related) ideographs.
+    /// This property roughly defines the class of "Chinese characters" and does not
+    /// include characters of other logographic scripts such as Cuneiform or Egyptian Hieroglyphs.
+    /// The Ideographic property is used in the definition of Ideographic Description Sequences.
     ideographic: BTreeSet<char>,
+
+    /// Used in Ideographic Description Sequences.
     ids_binary_operator: BTreeSet<char>,
+
+    /// Used in Ideographic Description Sequences.
     ids_trinary_operator: BTreeSet<char>,
+
+    /// Format control characters which have specific functions
+    /// for control of cursive joining and ligation.
     join_control: BTreeSet<char>,
+
+    /// A small number of spacing vowel letters occurring in certain Southeast Asian scripts
+    /// such as Thai and Lao, which use a visual order display model.
+    /// These letters are stored in text ahead of syllable-initial consonants,
+    /// and require special handling for processes such as searching and sorting.
     logical_order_exception: BTreeSet<char>,
+
+    /// Code points permanently reserved for internal use.
     noncharacter_code_point: BTreeSet<char>,
+
+    /// Used in deriving the Alphabetic property.
     other_alphabetic: BTreeSet<char>,
+
+    /// Used in deriving the `Default_Ignorable_Code_Point` property.
     other_default_ignorable_code_point: BTreeSet<char>,
+
+    /// Used in deriving the `Grapheme_Extend` property.
     other_grapheme_extend: BTreeSet<char>,
+
+    /// Used to maintain backward compatibility of `ID_Continue`.
     other_id_continue: BTreeSet<char>,
+
+    /// Used to maintain backward compatibility of `ID_Start`.
     other_id_start: BTreeSet<char>,
+
+    /// Used in deriving the `Lowercase` property.
     other_lowercase: BTreeSet<char>,
+
+    /// Used in deriving the `Math` property.
     other_math: BTreeSet<char>,
+
+    /// Used in deriving the `Uppercase` property.
     other_uppercase: BTreeSet<char>,
+
+    /// Used for pattern syntax as described in
+    /// Unicode Standard Annex #31, "Unicode Identifier and Pattern Syntax" ***[UAX31]***.
+    ///
+    /// [UAX31]: http://unicode.org/reports/tr41/tr41-21.html#UAX31
     pattern_syntax: BTreeSet<char>,
+
+    /// Used for pattern syntax as described in
+    /// Unicode Standard Annex #31, "Unicode Identifier and Pattern Syntax" ***[UAX31]***.
+    ///
+    /// [UAX31]: http://unicode.org/reports/tr41/tr41-21.html#UAX31
     pattern_white_space: BTreeSet<char>,
+
+    /// A small class of visible format controls, which precede and then span a
+    /// sequence of other characters, usually digits. These have also been known
+    /// as "subtending marks", because most of them take a form which visually
+    /// extends underneath the sequence of following digits.
     prepended_concatenation_mark: BTreeSet<char>,
+
+    /// Punctuation characters that function as quotation marks.
     quotation_mark: BTreeSet<char>,
+
+    /// Used in the definition of Ideographic Description Sequences.
     radical: BTreeSet<char>,
+
+    /// Property of the regional indicator characters, U+1F1E6-1F1FF.
+    /// This property is referenced in various segmentation algorithms,
+    /// to assist in correct breaking around emoji flag sequences.
     regional_indicator: BTreeSet<char>,
+
+    /// Punctuation characters that generally mark the end of sentences.
+    /// Used in Unicode Standard Annex #29, "Unicode Text Segmentation" ***[UAX29]***.
+    ///
+    /// [UAX29]: http://unicode.org/reports/tr41/tr41-21.html#UAX29
     sentence_terminal: BTreeSet<char>,
+
+    /// Characters with a "soft dot", like _i_ or _j_.
+    /// An accent placed on these characters causes the dot to disappear.
+    /// An explicit _dot above_ can be added where required, such as in Lithuanian.
+    /// See Section 7.1, Latin in ***[Unicode]***.
+    ///
+    /// [Unicode]: http://unicode.org/reports/tr41/tr41-21.html#Unicode
     soft_dotted: BTreeSet<char>,
+
+    /// Punctuation characters that generally mark the end of textual units.
     terminal_punctuation: BTreeSet<char>,
+
+    /// A property which specifies the exact set of Unified CJK Ideographs in the standard.
+    /// This set excludes CJK Compatibility Ideographs (which have canonical decompositions to
+    /// Unified CJK Ideographs), as well as characters from the CJK Symbols and Punctuation block.
+    /// The class of `Unified_Ideograph=Y` characters is a proper subset
+    /// of the class of `Ideographic=Y` characters.
     unified_ideograph: BTreeSet<char>,
+
+    /// Indicates characters that are Variation Selectors.
+    /// For details on the behavior of these characters,
+    /// see _Section 23.4, Variation Selectors_ in [Unicode],
+    /// and Unicode Technical Standard #37, "Unicode Ideographic Variation Database" [UTS37].
+    ///
+    /// [Unicode]: http://unicode.org/reports/tr41/tr41-21.html#Unicode
+    /// [UTS37]: http://unicode.org/reports/tr41/tr41-21.html#UTS37
     variation_selector: BTreeSet<char>,
+
+    /// Spaces, separator characters and other control characters which should be treated
+    /// by programming languages as "white space" for the purpose of parsing elements.
+    /// See also `Line_Break`, `Grapheme_Cluster_Break`, `Sentence_Break`, and `Word_Break`,
+    /// which classify space characters and related controls somewhat differently
+    /// for particular text segmentation contexts.
     white_space: BTreeSet<char>,
 }
 
