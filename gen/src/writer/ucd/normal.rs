7564880b7a8e5eefab368737d9bf6b983832cf1b
Behnam Esfahbod <behnam@zwnj.org>
2017-09-18 13:05:08 -0700

    [gen/writer/ucd/normal] Simplify emit functions

--- gen/src/writer/ucd/normal.rs
+++ gen/src/writer/ucd/normal.rs
@@ -12,8 +12,8 @@ use std::char;
 use std::collections::{BTreeMap, BTreeSet};
 use std::path::Path;
 
-use reader::ucd::unicode_data::{UnicodeDataEntry, UNICODE_DATA};
-use reader::ucd::derived_normalization_props::{CompositionExclusions, COMPOSITION_EXCLUSIONS};
+use reader::ucd::unicode_data::UNICODE_DATA;
+use reader::ucd::derived_normalization_props::COMPOSITION_EXCLUSIONS;
 use reader::ucd::readme::UNICODE_VERSION;
 
 use writer::utils::{capitalize, write};
@@ -23,118 +23,102 @@ use writer::common::unicode_version;
 
 pub fn generate(dir: &Path) {
     unicode_version::emit(&dir, &UNICODE_VERSION);
-
-    GeneralCategoryMarkData::from(UNICODE_DATA.entries.iter()).emit(dir);
-    CanonicalCombiningClassData::from(UNICODE_DATA.entries.iter()).emit(dir);
-
-    let decomposition = CanonicalDecompositionData::from(UNICODE_DATA.entries.iter());
-    decomposition.emit(dir);
-
-    CanonicalCompositionData::from(&decomposition, &COMPOSITION_EXCLUSIONS).emit(dir);
-    CompatibilityDecompositionData::from(UNICODE_DATA.entries.iter()).emit(dir);
+    general_category_mark_data_emit(dir);
+    canonical_combining_class_data_emit(dir);
+    canonical_decomposition_data_emit(dir);
+    canonical_composition_data_emit(dir);
+    compatibility_decomposition_data_emit(dir);
 }
 
 
-struct GeneralCategoryMarkData(BTreeSet<char>);
+fn general_category_mark_data_emit(dir: &Path) {
+    let set: BTreeSet<char> = UNICODE_DATA
+        .entries
+        .iter()
+        .filter(|x| {
+            matches!(x.general_category.as_str(), "Mn" | "Mc" | "Me")
+        })
+        .map(|x| x.character)
+        .collect();
 
-impl GeneralCategoryMarkData {
-    fn emit(&self, dir: &Path) {
-        let contents = self.0.to_range_char_set();
-        write(dir, "general_category_mark.rsv", &contents);
+    write(dir, "general_category_mark.rsv", &set.to_range_char_set());
+}
+
+
+fn canonical_combining_class_data_emit(dir: &Path) {
+    let map: BTreeMap<char, u8> = UNICODE_DATA
+        .entries
+        .iter()
+        .filter(|x| x.canonical_combining_class != 0)
+        .map(|x| (x.character, x.canonical_combining_class))
+        .collect();
+
+    write(
+        dir,
+        "canonical_combining_class_values.rsv",
+        &map.to_range_char_table(|val, f| write!(f, "CanonicalCombiningClass({})", val)),
+    );
+}
+
+
+fn decomposition_map<'a>() -> &'a BTreeMap<char, Box<[char]>> {
+    lazy_static!{
+        static ref MAP: BTreeMap<char, Box<[char]>> = {
+            UNICODE_DATA
+                .entries
+                .iter()
+                .filter(|x| {
+                    x.decomposition_type.is_none() && x.decomposition_mapping.is_some()
+                })
+                .map(|x| (x.character, x.decomposition_mapping.clone().unwrap()))
+                .collect()
+        };
     }
+    &MAP
 }
 
-impl<'a, I> From<I> for GeneralCategoryMarkData
-where
-    I: Iterator<Item = &'a UnicodeDataEntry>,
-{
-    fn from(it: I) -> Self {
-        let mut set = BTreeSet::new();
 
-        #[cfg_attr(rustfmt, rustfmt_skip)]
-        for &UnicodeDataEntry { character, ref general_category, .. } in it {
-            if matches!(general_category.as_str(), "Mn" | "Mc" | "Me") {
-                set.insert(character);
-            }
-        }
-
-        GeneralCategoryMarkData(set)
-    }
-}
-
-struct CanonicalCombiningClassData(BTreeMap<char, u8>);
-
-impl CanonicalCombiningClassData {
-    fn emit(&self, dir: &Path) {
-        let contents = self.0
-            .to_range_char_table(|val, f| write!(f, "CanonicalCombiningClass({})", val));
-        write(dir, "canonical_combining_class_values.rsv", &contents);
-    }
-}
-
-impl<'a, I> From<I> for CanonicalCombiningClassData
-where
-    I: Iterator<Item = &'a UnicodeDataEntry>,
-{
-    fn from(it: I) -> Self {
-        let mut map = BTreeMap::default();
-
-        #[cfg_attr(rustfmt, rustfmt_skip)]
-        for &UnicodeDataEntry { character, ref canonical_combining_class, .. } in it {
-            if *canonical_combining_class != 0 {
-                map.insert(character, *canonical_combining_class);
-            }
-        }
-
-        CanonicalCombiningClassData(map)
-    }
-}
-
-struct CanonicalDecompositionData<'a>(BTreeMap<char, &'a [char]>);
-
-impl<'a> CanonicalDecompositionData<'a> {
-    fn emit(&self, dir: &Path) {
-        let contents = self.0.to_direct_char_table(|val, f| {
+fn canonical_decomposition_data_emit(dir: &Path) {
+    write(
+        dir,
+        "canonical_decomposition_mapping.rsv",
+        &decomposition_map().to_direct_char_table(|raw, f| {
             write!(f, "&[")?;
-            for char in val.iter() {
+            for char in raw.iter() {
                 write!(f, "'{}',", char.escape_unicode())?;
             }
             write!(f, "]")
-        });
-        write(dir, "canonical_decomposition_mapping.rsv", &contents);
-    }
+        }),
+    );
 }
 
-impl<'a, I> From<I> for CanonicalDecompositionData<'a>
-where
-    I: Iterator<Item = &'a UnicodeDataEntry>,
-{
-    fn from(it: I) -> Self {
-        let mut map = BTreeMap::default();
 
-        for &UnicodeDataEntry {
-            character,
-            ref decomposition_type,
-            ref decomposition_mapping,
-            ..
-        } in it
-        {
-            if decomposition_type.is_none() {
-                if let Some(ref mapping) = *decomposition_mapping {
-                    map.insert(character, mapping.as_ref());
-                }
-            }
+fn canonical_composition_data_emit(dir: &Path) {
+    let mut map: BTreeMap<char, Vec<(char, char)>> = BTreeMap::default();
+
+    for (composed, decomposed) in decomposition_map() {
+        if decomposed.len() == 1 {
+            continue;
         }
-
-        CanonicalDecompositionData(map)
+        assert_eq!(decomposed.len(), 2);
+        if COMPOSITION_EXCLUSIONS.set.contains(&composed) {
+            continue;
+        }
+        let lead = decomposed[0];
+        let follow = decomposed[1];
+        map.entry(lead)
+            .or_insert_with(|| Vec::with_capacity(1))
+            .push((follow, *composed));
     }
-}
 
-struct CanonicalCompositionData(BTreeMap<char, Vec<(char, char)>>);
+    for value in map.values_mut() {
+        value.sort_by_key(|it| it.0)
+    }
 
-impl CanonicalCompositionData {
-    fn emit(&self, dir: &Path) {
-        let contents = self.0.to_direct_char_table(|val, f| {
+    write(
+        dir,
+        "canonical_composition_mapping.rsv",
+        &map.to_direct_char_table(|val, f| {
             write!(f, "CharDataTable::Direct(&[")?;
             for pair in val.iter() {
                 write!(
@@ -145,76 +129,34 @@ impl CanonicalCompositionData {
                 )?;
             }
             write!(f, "])")
-        });
-        write(dir, "canonical_composition_mapping.rsv", &contents);
-    }
-
-    fn from<'a>(
-        decomposition: &CanonicalDecompositionData<'a>,
-        exclusions: &CompositionExclusions,
-    ) -> CanonicalCompositionData {
-        let mut map = BTreeMap::default();
-        let &CanonicalDecompositionData(ref decomposition_map) = decomposition;
-
-        for (&composed, decomposed) in decomposition_map {
-            if decomposed.len() == 1 {
-                continue;
-            }
-            assert_eq!(decomposed.len(), 2);
-            if exclusions.set.contains(&composed) {
-                continue;
-            }
-            let lead = decomposed[0];
-            let follow = decomposed[1];
-            map.entry(lead)
-                .or_insert_with(|| Vec::with_capacity(1))
-                .push((follow, composed));
-        }
-
-        for value in map.values_mut() {
-            value.sort_by_key(|it| it.0)
-        }
-
-        CanonicalCompositionData(map)
-    }
+        }),
+    );
 }
 
-struct CompatibilityDecompositionData<'a>(BTreeMap<char, (&'a str, &'a [char])>);
 
-impl<'a> CompatibilityDecompositionData<'a> {
-    fn emit(&self, dir: &Path) {
-        let contents = self.0.to_direct_char_table(|val, f| {
+fn compatibility_decomposition_data_emit(dir: &Path) {
+    let map: BTreeMap<char, (&str, &[char])> = UNICODE_DATA
+        .entries
+        .iter()
+        .filter_map(|x| {
+            if let Some(ref dt) = x.decomposition_type {
+                if let Some(ref mapping) = x.decomposition_mapping {
+                    return Some((x.character, (dt.as_str(), mapping.as_ref())));
+                }
+            }
+            None
+        })
+        .collect();
+
+    write(
+        dir,
+        "compatibility_decomposition_mapping.rsv",
+        &map.to_direct_char_table(|val, f| {
             write!(f, "({}, &[", capitalize(&val.0.to_lowercase()))?;
             for char in val.1.iter() {
                 write!(f, "'{}',", char.escape_unicode()).unwrap();
             }
             write!(f, "])")
-        });
-        write(dir, "compatibility_decomposition_mapping.rsv", &contents);
-    }
-}
-
-impl<'a, I> From<I> for CompatibilityDecompositionData<'a>
-where
-    I: Iterator<Item = &'a UnicodeDataEntry>,
-{
-    fn from(it: I) -> Self {
-        let mut map = BTreeMap::default();
-
-        for &UnicodeDataEntry {
-            character,
-            ref decomposition_type,
-            ref decomposition_mapping,
-            ..
-        } in it
-        {
-            if let Some(ref typ) = *decomposition_type {
-                if let Some(ref mapping) = *decomposition_mapping {
-                    map.insert(character, (typ.as_str(), mapping.as_ref()));
-                }
-            }
-        }
-
-        CompatibilityDecompositionData(map)
-    }
+        }),
+    );
 }
