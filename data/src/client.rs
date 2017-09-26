517af71fa80cf765a271dde4fd11716a01706623
Behnam Esfahbod <behnam@zwnj.org>
2017-09-25 20:57:29 -0700

    [data] Move over download code
    Create `unic-data` crate, hosted under `/data/`, which is responsible
    for downloading the files from sources, as set in `sources.toml`.
    
    This makes the `/data/` *component* standalone, by separating the matter
    of downloading data from the processing and table generation, which is
    the responsibility of `data-gen` crate.

---
+++ data/src/client.rs
@@ -0,0 +1,58 @@
+// Copyright 2017 The UNIC Project Developers.
+//
+// See the COPYRIGHT file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+
+use std::error::Error;
+use std::fs::File;
+use std::io::Write;
+
+use futures::{Future, Stream};
+use futures::future::join_all;
+use hyper::{Client, Uri};
+use tokio_core::reactor::Core;
+
+use config::DownloadMap;
+
+
+/// Concurrently download all files as indicated by `DownloadMap`s.
+///
+/// Returns once all jobs are finished.
+///
+/// # Panics
+///
+/// If a file that is supposed to be written cannot be created and opened
+///
+/// # Errors
+///
+/// If a error occurs while downloading or writing the files
+pub fn download_all(download_paths: &[DownloadMap]) -> Result<(), Box<Error>> {
+    let mut core = Core::new()?;
+    let client = Client::new(&core.handle());
+
+    let jobs = download_paths.iter().map(move |path| {
+        path.url
+            .parse::<Uri>()
+            .map(|uri| {
+                client.get(uri).and_then(move |res| {
+                    File::create(&path.dest)
+                        .map(move |mut file| {
+                            res.body()
+                                .for_each(move |chunk| file.write_all(&chunk).map_err(From::from))
+                        })
+                        .expect("Failed to create file")
+                })
+            })
+            .expect("Invalid URI")
+    });
+
+    core.run(join_all(jobs))?;
+
+    Ok(())
+}
---
+++ data/src/main.rs
@@ -0,0 +1,88 @@
+// Copyright 2017 The UNIC Project Developers.
+//
+// See the COPYRIGHT file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+
+//! UNIC - Source Data Download Script
+//!
+//! # Config Format:
+//!
+//! ```toml
+//! [example]
+//! version = "10.0.0"
+//! base_url = "http://example.com/path/to/{version}/"
+//!
+//! [example.resources]
+//! "some_data.txt" = "some_data.txt"
+//!
+//! [example.test_resources]
+//! "other_data.txt" = "tests/other_data.txt"
+//! ```
+
+
+#[macro_use]
+extern crate clap;
+
+extern crate futures;
+extern crate hyper;
+extern crate tokio_core;
+
+#[macro_use]
+extern crate serde_derive;
+
+extern crate toml;
+
+
+mod client;
+mod config;
+
+
+use std::fs;
+
+use config::DownloadMap;
+
+
+fn main() {
+    let matches = clap_app!(
+        unic_data =>
+            (author: "The UNIC Project Developers")
+            (about: "Download data files based on sources.toml configuration")
+            (@arg sources: ... "Sources to download data from, or all if not set")
+    ).get_matches();
+
+    let sources: Vec<_> = match matches.values_of("sources") {
+        Some(s) => s.collect(),
+        None => vec![],
+    };
+    let download_maps = config::get_download_maps(&sources);
+
+    println!("Cleaning destination directories...");
+    clean_dirs(&download_maps);
+
+    println!("Downloading {} files:", download_maps.len());
+    for dmap in download_maps.iter() {
+        println!("    {} => {}", &dmap.url, dmap.dest.display());
+    }
+
+    client::download_all(&download_maps).expect("Download error");
+}
+
+
+fn clean_dirs(download_maps: &[DownloadMap]) {
+    let dirs = download_maps
+        .iter()
+        .map(|ref dmap| dmap.dest.parent().expect("Bad destination path"));
+    for dir in dirs {
+        let _ = fs::remove_dir_all(dir); // ignore errors
+        fs::create_dir_all(dir).expect(&format!(
+            "Error creating destination directory: {}",
+            dir.display()
+        ));
+    }
+}
