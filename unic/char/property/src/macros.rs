a06430ddf80962331b27aeafc39d1feba7c5a76c
Behnam Esfahbod <behnam@zwnj.org>
2017-10-02 01:16:08 -0700

    [char/property] Rename macro internals
    Just a simple search and replace to improve readability of internal
    implementations, specially to distinguish between names for properties
    themselves vs. names for property values.

--- unic/char/property/src/macros.rs
+++ unic/char/property/src/macros.rs
@@ -102,18 +102,18 @@ macro_rules! char_property {
     // == Enumerated Property == //
 
     (
-        $(#[$name_meta:meta])*
-        pub enum $name:ident {
-            abbr => $abbr_name:expr;
-            long => $long_name:expr;
-            human => $human_name:expr;
+        $(#[$prop_meta:meta])*
+        pub enum $prop_name:ident {
+            abbr => $prop_abbr:expr;
+            long => $prop_long:expr;
+            human => $prop_human:expr;
 
             $(
                 $(#[$variant_meta:meta])+
-                | $variant:ident {
-                    abbr => $abbr:ident,
-                    long => $long:ident,
-                    human => $human:expr,
+                | $variant_name:ident {
+                    abbr => $variant_abbr:ident,
+                    long => $variant_long:ident,
+                    human => $variant_human:expr,
                 }
             )*
         }
@@ -125,57 +125,65 @@ macro_rules! char_property {
         pub mod $long_mod:ident for long;
 
     ) => {
-        $(#[$name_meta])*
+        $(#[$prop_meta])*
         #[allow(bad_style)]
         #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
-        pub enum $name {
-            $( $(#[$variant_meta])+ $variant, )*
+        pub enum $prop_name {
+            $( $(#[$variant_meta])+ $variant_name, )*
         }
 
         $(#[$abbr_mod_meta])*
         #[allow(bad_style)]
         pub mod $abbr_mod {
-            $( pub use super::$name::$variant as $abbr; )*
+            $( pub use super::$prop_name::$variant_name as $variant_abbr; )*
         }
 
         $(#[$long_mod_meta])*
         #[allow(bad_style)]
         pub mod $long_mod {
-            $( pub use super::$name::$variant as $long; )*
+            $( pub use super::$prop_name::$variant_name as $variant_long; )*
         }
 
         char_property! {
-            __impl FromStr for $name;
+            __impl FromStr for $prop_name;
             $(
-                $abbr => $name::$variant;
-                $long => $name::$variant;
+                $variant_abbr => $prop_name::$variant_name;
+                $variant_long => $prop_name::$variant_name;
             )*
         }
 
         char_property! {
-            __impl CharProperty for $name;
-            $abbr_name;
-            $long_name;
-            $human_name;
+            __impl CharProperty for $prop_name;
+            $prop_abbr;
+            $prop_long;
+            $prop_human;
         }
 
         char_property! {
-            __impl Display for $name by EnumeratedCharProperty
+            __impl Display for $prop_name by EnumeratedCharProperty
         }
 
-        impl $crate::EnumeratedCharProperty for $name {
-            fn all_values() -> &'static [$name] {
-                const VALUES: &[$name] = &[ $($name::$variant,)+ ];
+        impl $crate::EnumeratedCharProperty for $prop_name {
+            fn all_values() -> &'static [$prop_name] {
+                const VALUES: &[$prop_name] = &[
+                    $( $prop_name::$variant_name, )+
+                ];
                 VALUES
             }
             fn abbr_name(&self) -> &'static str {
-                match *self { $( $name::$variant => stringify!($abbr), )* }
+                match *self {
+                    $( $prop_name::$variant_name => stringify!($variant_abbr), )*
+                }
             }
             fn long_name(&self) -> &'static str {
-                match *self { $( $name::$variant => stringify!($long), )* }
+                match *self {
+                    $( $prop_name::$variant_name => stringify!($variant_long), )*
+                }
             }
             fn human_name(&self) -> &'static str {
-                match *self { $( $name::$variant => $human, )* }
+                match *self {
+                    $( $prop_name::$variant_name => $variant_human, )*
+                }
             }
         }
     };
@@ -183,11 +191,11 @@ macro_rules! char_property {
     // == Binary Property == //
 
     (
-        $(#[$name_meta:meta])*
-        pub struct $name:ident(bool) {
-            abbr => $abbr_name:expr;
-            long => $long_name:expr;
-            human => $human_name:expr;
+        $(#[$prop_meta:meta])*
+        pub struct $prop_name:ident(bool) {
+            abbr => $prop_abbr:expr;
+            long => $prop_long:expr;
+            human => $prop_human:expr;
 
             data_table_path => $data_path:expr;
         }
@@ -196,21 +204,21 @@ macro_rules! char_property {
         pub fn $is_fn:ident(char) -> bool;
 
     ) => {
-        $(#[$name_meta])*
+        $(#[$prop_meta])*
         #[derive(Copy, Clone, Debug, Default, Eq, PartialEq, Hash)]
-        pub struct $name(bool);
+        pub struct $prop_name(bool);
 
         $(#[$is_fn_meta])*
         pub fn $is_fn(ch: char) -> bool {
-            $name::of(ch).as_bool()
+            $prop_name::of(ch).as_bool()
         }
 
-        impl $name {
+        impl $prop_name {
             /// Get (struct) property value of the character.
             pub fn of(ch: char) -> Self {
                 use $crate::unic_utils::CharDataTable;
                 const TABLE: CharDataTable<()> = include!($data_path);
-                $name(TABLE.contains(ch))
+                $prop_name(TABLE.contains(ch))
             }
 
             /// Get boolean property value of the character.
@@ -218,64 +226,64 @@ macro_rules! char_property {
         }
 
         char_property! {
-            __impl FromStr for $name;
+            __impl FromStr for $prop_name;
             // Yes
-            y => $name(true);
-            yes => $name(true);
-            t => $name(true);
-            true => $name(true);
+            y => $prop_name(true);
+            yes => $prop_name(true);
+            t => $prop_name(true);
+            true => $prop_name(true);
             // No
-            n => $name(false);
-            no => $name(false);
-            f => $name(false);
-            false => $name(false);
+            n => $prop_name(false);
+            no => $prop_name(false);
+            f => $prop_name(false);
+            false => $prop_name(false);
         }
 
         char_property! {
-            __impl CharProperty for $name;
-            $abbr_name;
-            $long_name;
-            $human_name;
+            __impl CharProperty for $prop_name;
+            $prop_abbr;
+            $prop_long;
+            $prop_human;
         }
 
-        impl $crate::TotalCharProperty for $name {
+        impl $crate::TotalCharProperty for $prop_name {
             fn of(ch: char) -> Self { Self::of(ch) }
         }
 
-        impl $crate::BinaryCharProperty for $name {
+        impl $crate::BinaryCharProperty for $prop_name {
             fn bool(&self) -> bool { self.as_bool() }
         }
 
-        impl From<$name> for bool {
-            fn from(prop: $name) -> bool { prop.as_bool() }
+        impl From<$prop_name> for bool {
+            fn from(prop: $prop_name) -> bool { prop.as_bool() }
         }
 
         char_property! {
-            __impl Display for $name by BinaryCharProperty
+            __impl Display for $prop_name by BinaryCharProperty
         }
     };
 
     // == Shared == //
 
     (
-        __impl CharProperty for $name:ident;
-        $abbr:expr;
-        $long:expr;
-        $human:expr;
+        __impl CharProperty for $prop_name:ident;
+        $prop_abbr:expr;
+        $prop_long:expr;
+        $prop_human:expr;
     ) => {
-        impl $crate::CharProperty for $name {
-            fn prop_abbr_name() -> &'static str { $abbr }
-            fn prop_long_name() -> &'static str { $long }
-            fn prop_human_name() -> &'static str { $human }
+        impl $crate::CharProperty for $prop_name {
+            fn prop_abbr_name() -> &'static str { $prop_abbr }
+            fn prop_long_name() -> &'static str { $prop_long }
+            fn prop_human_name() -> &'static str { $prop_human }
         }
     };
 
     (
-        __impl FromStr for $name:ident;
+        __impl FromStr for $prop_name:ident;
         $( $id:ident => $value:expr; )*
     ) => {
         #[allow(unreachable_patterns)]
-        impl ::std::str::FromStr for $name {
+        impl ::std::str::FromStr for $prop_name {
             type Err = ();
             fn from_str(s: &str) -> Result<Self, Self::Err> {
                 match s {
@@ -291,8 +299,8 @@ macro_rules! char_property {
         }
     };
 
-    ( __impl Display for $name:ident by $trait:ident ) => {
-        impl ::std::fmt::Display for $name {
+    ( __impl Display for $prop_name:ident by $trait:ident ) => {
+        impl ::std::fmt::Display for $prop_name {
             fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
                 $crate::$trait::human_name(self).fmt(f)
             }
