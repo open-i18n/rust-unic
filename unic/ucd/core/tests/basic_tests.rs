886d358a1e073f66dcbbb52729c9ab10bfeae35e
Behnam Esfahbod <behnam@zwnj.org>
2017-11-05 17:56:49 -0800

    [ucd/core] Make library no_std
    Testing `fmt::Display` under `no_std` can get too much overhead,
    therefore it's moved to `tests/` so it can use `std::string` and
    friends.

---
+++ unic/ucd/core/tests/basic_tests.rs
@@ -0,0 +1,30 @@
+// Copyright 2017 The UNIC Project Developers.
+//
+// See the COPYRIGHT file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+
+extern crate unic_ucd_core;
+
+use unic_ucd_core::UnicodeVersion;
+
+
+#[test]
+fn test_display() {
+    assert_eq!(
+        format!(
+            "Unicode {}",
+            UnicodeVersion {
+                major: 1,
+                minor: 2,
+                micro: 0,
+            }
+        ),
+        "Unicode 1.2.0"
+    );
+}
