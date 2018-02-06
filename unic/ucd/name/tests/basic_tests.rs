7f4d3be57c83a8156772b70135b3561b4c4df9bb
Yan Li <eyeplum@gmail.com>
2018-02-06 14:48:42 +1300

    add tests for name mapping

--- unic/ucd/name/tests/basic_tests.rs
+++ unic/ucd/name/tests/basic_tests.rs
@@ -18,6 +18,26 @@ fn basic_tests() {
         Name::of('A').expect("No name for A").to_string(),
         "LATIN CAPITAL LETTER A"
     );
+    assert_eq!(
+        Name::of('곲').expect("No name for 곲").to_string(),
+        "HANGUL SYLLABLE ACF2" // FIXME: use decomposed jamo short names instead
+    );
+    assert_eq!(
+        Name::of('龑').expect("No name for 龑").to_string(),
+        "CJK UNIFIED IDEOGRAPH-9F91"
+    );
+    assert_eq!(
+        Name::of('\u{17005}').expect("No name for U+17005").to_string(),
+        "TANGUT IDEOGRAPH-17005"
+    );
+    assert_eq!(
+        Name::of('\u{1B175}').expect("No name for U+1B175").to_string(),
+        "NUSHU CHARACTER-1B175"
+    );
+    assert_eq!(
+        Name::of('理').expect("No name for 理").to_string(),
+        "CJK COMPATIBILITY IDEOGRAPH-F9E4"
+    );
     assert_eq!(Name::of('\u{10FFFF}'), None);
 
     assert!(Name::of('A') < Name::of('B'));
