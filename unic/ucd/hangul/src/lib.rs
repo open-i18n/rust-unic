96a4724ace5ca3ce922ad5d1692c6ae2c4d32da7
Yan Li <eyeplum@gmail.com>
2018-03-14 17:13:24 +1300

    [ucd/hangul] add examples

--- unic/ucd/hangul/src/lib.rs
+++ unic/ucd/hangul/src/lib.rs
@@ -13,6 +13,35 @@
 #![forbid(future_incompatible, missing_debug_implementations, unconditional_recursion, unsafe_code)]
 #![deny(bad_style, unsafe_code, unused)]
 
+//! # UNIC - UCD - Hangul Syllable Composition & Decomposition
+//!
+//! This UCD component provides algorithms described in [Unicode®
+//! Standard - 3.12 Conjoining Jamo Behavior](https://www.unicode.org/versions/latest/ch03.pdf),
+//! used for detecting, composing and decomposing Hangul syllables.
+//!
+//! # Examples
+//!
+//! ```rust
+//! # use unic_ucd_hangul::{is_syllable, compose_syllable, decompose_syllable};
+//!
+//! assert!(is_syllable('갏'));
+//! assert!(!is_syllable('A'));
+//!
+//! let mut decomposed_lv = vec![];
+//! {
+//!     let mut collect_decomposed = |chr| {
+//!         decomposed_lv.push(chr);
+//!     };
+//!     decompose_syllable('쮀', &mut collect_decomposed);
+//! }
+//! assert_eq!(decomposed_lv, ['ᄍ', 'ᅰ']);
+//!
+//! assert_eq!(
+//!     compose_syllable('ᄑ', 'ᅱ').unwrap(),
+//!     '퓌',
+//! );
+//! ```
+
 extern crate unic_ucd_version;
 use unic_ucd_version::UnicodeVersion;
 
