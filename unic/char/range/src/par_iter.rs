09c9ebe9b741ad087bbd921fe35e0a123aaed3ce
Christopher Durham <cad97@cad97.com>
2018-04-15 18:13:09 -0400

    IntoParallelIterator for CharRange

--- unic/char/range/src/par_iter.rs
+++ unic/char/range/src/par_iter.rs
@@ -1,10 +1,26 @@
 extern crate rayon;
 
-use rayon::prelude::*;
+use core::char;
+use self::rayon::prelude::*;
 use CharRange;
 
+#[derive(Clone, Debug)]
+pub struct Iter(rayon::iter::FilterMap<rayon::range::Iter<u32>, fn(u32) -> Option<char>>);
+
+impl ParallelIterator for Iter {
+    type Item = char;
+    fn drive_unindexed<C>(self, consumer: C) -> C::Result
+    where
+        C: rayon::iter::plumbing::UnindexedConsumer<Self::Item>,
+    {
+        self.0.drive_unindexed(consumer)
+    }
+}
+
 impl IntoParallelIterator for CharRange {
+    type Iter = Iter;
+    type Item = char;
     fn into_par_iter(self) -> Self::Iter {
-        compile_error!("")
+        Iter(((self.low as u32)..(self.high as u32 + 1)).into_par_iter().filter_map(char::from_u32))
     }
 }
