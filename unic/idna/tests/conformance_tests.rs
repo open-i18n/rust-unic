38ea9f45ec5610672009af2f418a775467e8b50c
Alex Macleod <alex@macleod.io>
2018-07-27 19:00:56 +0100

    Update IDNA conformance test for v2 test data
    Additionally, fix a bug in the previous implemantation where the test
    returns early in many situations before reaching the end of file

--- unic/idna/tests/conformance_tests.rs
+++ unic/idna/tests/conformance_tests.rs
@@ -12,8 +12,8 @@ use std::char;
 
 #[test]
 fn test_idn_test_data() {
-    // Source: https://www.unicode.org/Public/idna/latest/IdnaTest.txt
-    for (line_idx, line) in include_str!("../../../data/idna/test/IdnaTest.txt")
+    // Source: https://www.unicode.org/Public/idna/latest/IdnaTestV2.txt
+    for (line_idx, line) in include_str!("../../../data/idna/test/IdnaTestV2.txt")
         .lines()
         .enumerate()
     {
@@ -27,119 +27,128 @@ fn test_idn_test_data() {
             None => line,
         };
 
-        let mut expected_failure = false;
-        if line.starts_with("XFAIL") {
-            expected_failure = true;
-            line = &line[5..line.len()];
-        };
+        let mut pieces = line.split(';').map(|x| x.trim());
 
-        let mut pieces = line.split(';').map(|x| x.trim()).collect::<Vec<&str>>();
+        let source = pieces.next().unwrap();
+        let to_unicode = empty_or(&mut pieces, source);
+        let to_unicode_status = empty_or(&mut pieces, "[]");
+        let to_ascii_n = empty_or(&mut pieces, to_unicode);
+        let to_ascii_n_status = empty_or(&mut pieces, to_unicode_status);
+        let to_ascii_t = empty_or(&mut pieces, to_ascii_n);
+        let to_ascii_t_status = empty_or(&mut pieces, to_ascii_n_status);
 
-        let test_type = pieces.remove(0);
-        let original = pieces.remove(0);
-        let source = unescape(original);
-        let to_unicode = pieces.remove(0);
-        let to_ascii = pieces.remove(0);
-        let nv8 = if !pieces.is_empty() {
-            pieces.remove(0)
-        } else {
-            ""
-        };
-
-        if expected_failure {
-            continue;
-        }
-
-        let test_name = format!("IdnaTest:{}", line_idx + 1);
-
-        let result = unic_idna::to_ascii(
-            &source,
-            unic_idna::Flags {
-                use_std3_ascii_rules: true,
-                transitional_processing: test_type == "T",
-                verify_dns_length: true,
-            },
-        );
-
-        if to_ascii.starts_with('[') {
-            if to_ascii.starts_with("[C") {
-                // http://unicode.org/reports/tr46/#Deviations
-                // applications that perform IDNA2008 lookup are not required to check
-                // for these contexts
-                return;
-            }
-            if to_ascii == "[V2]" {
-                // Everybody ignores V2
-                // https://github.com/servo/rust-url/pull/240
-                // https://github.com/whatwg/url/issues/53#issuecomment-181528158
-                // https://www.unicode.org/review/pri317/
-                return;
-            }
-            let res = result.ok();
-            assert_eq!(
-                res, None,
-                "{}: Expected error for source `{}`. original: `{}`",
-                test_name, source, original
-            );
-            return;
-        }
-
-        let to_ascii = if !to_ascii.is_empty() {
-            to_ascii.to_string()
-        } else if !to_unicode.is_empty() {
-            to_unicode.to_string()
-        } else {
-            source.clone()
-        };
-
-        if nv8 == "NV8" {
-            // This result isn't valid under IDNA2008. Skip it
-            return;
-        }
-
-        assert!(
-            result.is_ok(),
-            "{}: Couldn't parse source `{}`. original: `{}`. error: `{:?}`",
-            test_name,
-            source,
-            original,
-            result.err()
-        );
-        let output = result.unwrap();
-        assert_eq!(
-            output, to_ascii,
-            "{}: Incorrect result for source `{}`. original: `{}`",
-            test_name, source, original
-        );
+        assert_unicode(source, to_unicode, to_unicode_status, line_idx);
+        assert_ascii(source, to_ascii_n, to_ascii_n_status, false, line_idx);
+        assert_ascii(source, to_ascii_t, to_ascii_t_status, true, line_idx);
     }
 }
 
-fn unescape(input: &str) -> String {
-    let mut output = String::new();
-    let mut chars = input.chars();
-    loop {
-        match chars.next() {
-            None => return output,
-            Some(c) => if c == '\\' {
-                match chars.next().unwrap() {
-                    '\\' => output.push('\\'),
-                    'u' => {
-                        let c1 = chars.next().unwrap().to_digit(16).unwrap();
-                        let c2 = chars.next().unwrap().to_digit(16).unwrap();
-                        let c3 = chars.next().unwrap().to_digit(16).unwrap();
-                        let c4 = chars.next().unwrap().to_digit(16).unwrap();
-                        match char::from_u32(((c1 * 16 + c2) * 16 + c3) * 16 + c4) {
-                            Some(c) => output.push(c),
-                            None => {
-                                output.push_str(&format!("\\u{:X}{:X}{:X}{:X}", c1, c2, c3, c4));
-                            }
-                        };
-                    }
-                    _ => panic!("Invalid test data input"),
-                }
-            } else {
-                output.push(c);
-            },
+fn empty_or<'a>(iter: &mut Iterator<Item = &'a str>, or: &'a str) -> &'a str {
+    let next = iter.next().unwrap();
+
+    if next.is_empty() {
+        or
+    } else {
+        next
+    }
+}
+
+fn assert_unicode(source: &str, expected: &str, status: &str, line_idx: usize)
+{
+    let (actual, result) = unic_idna::to_unicode(
+        source,
+        unic_idna::Flags {
+            use_std3_ascii_rules: true,
+            transitional_processing: false,
+            verify_dns_length: true,
+        },
+    );
+
+    if let Ok(()) = result {
+        assert_eq!(
+            actual,
+            expected,
+            "data/idna/test/IdnaTestV2.txt:{}",
+            line_idx + 1
+        );
+
+        if status.starts_with("[C") {
+            // http://unicode.org/reports/tr46/#Deviations
+            // applications that perform IDNA2008 lookup are not required to check
+            // for these contexts
+            return;
         }
+
+        if status == "[V2]" {
+            // Everybody ignores V2
+            // https://github.com/servo/rust-url/pull/240
+            // https://github.com/whatwg/url/issues/53#issuecomment-181528158
+            // https://www.unicode.org/review/pri317/
+            return;
+        }
+
+        assert_eq!(
+            status,
+            "[]",
+            "data/idna/test/IdnaTestV2.txt:{}",
+            line_idx + 1
+        );
+    } else {
+        assert_ne!(
+            status,
+            "[]",
+            "data/idna/test/IdnaTestV2.txt:{}",
+            line_idx + 1
+        );
+    }
+
+}
+
+fn assert_ascii(source: &str, expected: &str, status: &str, transitional: bool, line_idx: usize) {
+    let result = unic_idna::to_ascii(
+        source,
+        unic_idna::Flags {
+            use_std3_ascii_rules: true,
+            transitional_processing: transitional,
+            verify_dns_length: true,
+        },
+    );
+
+    if let Ok(actual) = result {
+        assert_eq!(
+            actual,
+            expected,
+            "data/idna/test/IdnaTestV2.txt:{}",
+            line_idx + 1
+        );
+
+        if status.starts_with("[C") {
+            // http://unicode.org/reports/tr46/#Deviations
+            // applications that perform IDNA2008 lookup are not required to check
+            // for these contexts
+            return;
+        }
+
+        if status == "[V2]" {
+            // Everybody ignores V2
+            // https://github.com/servo/rust-url/pull/240
+            // https://github.com/whatwg/url/issues/53#issuecomment-181528158
+            // https://www.unicode.org/review/pri317/
+            return;
+        }
+
+        assert_eq!(
+            status,
+            "[]",
+            "data/idna/test/IdnaTestV2.txt:{}",
+            line_idx + 1
+        );
+    } else {
+        assert_ne!(
+            status,
+            "[]",
+            "data/idna/test/IdnaTestV2.txt:{}",
+            line_idx + 1
+        );
     }
 }
