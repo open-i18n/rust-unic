8106816819e9ef3cfa3e3376434575d864f6af35
Yan Li <eyeplum@gmail.com>
2018-10-06 21:06:40 +1300

    [ucd/name_aliases] Add missing exports

--- unic/ucd/src/lib.rs
+++ unic/ucd/src/lib.rs
@@ -35,6 +35,7 @@ pub use unic_ucd_category as category;
 pub use unic_ucd_hangul as hangul;
 pub use unic_ucd_ident as ident;
 pub use unic_ucd_name as name;
+pub use unic_ucd_name_aliases as name_aliase;
 pub use unic_ucd_normal as normal;
 pub use unic_ucd_segment as segment;
 
@@ -77,6 +78,14 @@ pub use crate::common::{is_alphabetic, is_white_space, Alphabetic, WhiteSpace};
 pub use crate::name::Name;
 
 pub use crate::normal::CanonicalCombiningClass;
+
+pub use crate::name_aliase::{
+    alternate_names_of,
+    control_code_names_of,
+    figments_of,
+    name_corrections_of,
+    name_abbreviations_of,
+};
 
 pub use crate::segment::{GraphemeClusterBreak, SentenceBreak, WordBreak};
 
